a82bf7ed5f57b7681f2ced1d3821e71ac47f73fd
JannisMueller <jannis.mueller@hotmail.de>
2021-02-26 10:45:44 +0100

    adding Logger

--- src/main/java/se/mueller/webservice/controllers/DirectorController.java
+++ src/main/java/se/mueller/webservice/controllers/DirectorController.java
@@ -1,6 +1,8 @@
 package se.mueller.webservice.controllers;
 
 import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +21,11 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 @RestController
 public class DirectorController {
+
+    Logger log = LoggerFactory.getLogger(DirectorController.class);
 
     private final Service service;
 
@@ -30,21 +35,24 @@ public class DirectorController {
 
     @GetMapping("/directors")
     public List<Directordto> all(Long id) {
+        log.info("Get all directors");
         return service.getAllDirectors();
     }
 
     @GetMapping("/directors/{id}")
     public Optional <Directordto> getOne(@PathVariable Long id){
-
+        log.info("Get director with id " + id);
         return Optional.ofNullable(service.getOne(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Id " + id + " not found.")));
+
 
     }
 
     @PostMapping("/directors")
     @ResponseStatus(HttpStatus.CREATED)
     public Directordto create(@RequestBody Directordto directorDto) {
+        log.info("New Director created");
         return service.createDirector(directorDto);
     }
 
@@ -52,24 +60,29 @@ public class DirectorController {
     @DeleteMapping("/directors/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
+        log.info("Director with id " + id + " deleted");
     }
 
     @PutMapping("/directors/{id}")
     public Directordto replace(@RequestBody Directordto directorDto, @PathVariable Long id) {
+        log.info("Director with id " + id + " replaced");
         return service.replace(id, directorDto);
+
     }
 
     @PatchMapping("/directors/{id}")
     public Directordto update(@RequestBody DirectorNationality nationality, @PathVariable Long id) {
+        log.info("Director with id " + id + " updated");
         return service.update(id, nationality);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/directors/search")
     public List<Directordto> search(@RequestParam(value = "firstName") String search) {
+        log.info("Gets all Directors with name " + search);
 
         return service.findAllBySpec(search);
 
-    };
+    }
 }
 
 
